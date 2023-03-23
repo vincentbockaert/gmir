fbaa7fac24d2db0bff78dbfea32b30f87a8e0e43
Vincent Bockaert <vincent.bockaert@proton.me>
2023-03-23 18:32:24 +0100

    Remove boilerplate rootCmd flag

--- cmd/root.go
+++ cmd/root.go
@@ -27,5 +27,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
